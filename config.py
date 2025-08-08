39906dc68923b791f9c9756411deadb72698af30
ornilio <orniliooficial@outlook.com>
2025-08-08 09:02:06 -0300

    correcoa no camiho do banco no config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'minha-chave-secreta-padrao'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
