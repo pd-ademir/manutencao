fcbc2d5163ef8b2ab9d0943059dceafa840b8f8a
ornilio <orniliooficial@outlook.com>
2025-09-18 15:49:54 -0300

    ajuste para atualizar km pelo celular

--- run.py
+++ run.py
@@ -8,3 +8,9 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=1)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # Adicionado host='0.0.0.0' para aceitar conexões da sua rede local
+   # app.run(debug=True, host='0.0.0.0')
+
+
+
+
